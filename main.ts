1389afa0faeac9f05918a6b78d68f7f1c7be0ac6
karaimelinda <80882802+karaimelinda@users.noreply.github.com>
2021-04-22 18:38:20 +0200

    Első verzió

--- main.ts
+++ main.ts
@@ -1,14 +1,31 @@
-let fegyver = 0
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    saját += 1
+    basic.showString("S:")
+    basic.showString("" + (saját))
+})
 input.onGesture(Gesture.Shake, function () {
     fegyver = randint(0, 2)
     if (fegyver == 0) {
         basic.showIcon(IconNames.SmallSquare)
+        basic.showString("Hello!")
     } else if (fegyver == 1) {
         basic.showIcon(IconNames.Square)
     } else {
         basic.showIcon(IconNames.Scissors)
     }
 })
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    barát += 1
+    basic.showString("B:")
+    basic.showString("" + (barát))
+})
+let fegyver = 0
+let barát = 0
+let saját = 0
+saját = 0
+barát = 0
 basic.forever(function () {
 	
 })
